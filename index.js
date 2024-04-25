67f62d1302b06a976af141fefafc6e173aa8788b
armeen912 <164738082+armeen912@users.noreply.github.com>
2024-04-25 16:26:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 2,
+    EUR: 0.91,
+    GBR: 0.76,
+    INR: 74.54,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+    { name: 'form',
+        message: 'Enter From Currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBR', 'INR', 'PKR'],
+    },
+    { name: 'to',
+        message: 'Enter To Currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBR', 'INR', 'PKR'],
+    },
+    { name: 'amout',
+        message: 'Enter You Amount',
+        type: 'list',
+    },
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD BASE  CURRENCY //4
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
